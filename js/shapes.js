00b7db831757e13cf6a4aee8a4fcfa8e8469cea4
Ellis Halliday <1478pop@gmail.com>
2019-12-10 13:18:01 -0500

    Missing 4

--- js/shapes.js
+++ js/shapes.js
@@ -21,6 +21,8 @@ window.onload = function() {
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
     document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
+    document.getElementById("draw-triangle").onclick = drawRectangle;
+    document.getElementById("smile").onclick = drawFace;
 }
 
 /*
@@ -106,11 +108,27 @@ const drawRectangle = function() {
  */
 
 const drawColoredRectangle = function() {
+  // write your exercise 3 code here
 
-paul = alert("hi")
+const canvas = document.getElementById('student-canvas-3');
+const ctx = canvas.getContext('2d');
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+do {
+  var color = (prompt("Color: "))
+  if (color == null) {
+    break;
+  }
+  color = String(color)
+  var color_case = color.toLowerCase()
+  if (color_case != "green" && color_case != "black" && color_case != "blue" && color_case != "orange" && color_case != "purple" && color_case != "red" && color_case != "yellow") {
+    alert(color + " is not a supported color.")
+  }
+} while (color_case != "green" && color_case != "black" && color_case != "blue" && color_case != "orange" && color_case != "purple" && color_case != "red" && color_case != "yellow")
 
-    // write your exercise 3 code here
-
+if (color != null) {
+  ctx.fillStyle = color_case;
+  ctx.fillRect(10, 10, 100, 50);
+}
 };
 
 /*
@@ -119,9 +137,9 @@ paul = alert("hi")
 
 const drawTriangle = function() {
 
-
-
     // write your exercise 4 code here
+    const canvas = document.getElementById('student-canvas-4');
+    const ctx = canvas.getContext('2d');
 
 };
 
@@ -130,10 +148,48 @@ const drawTriangle = function() {
  */
 
 const drawFace = function() {
+    // write your exercise 5 code here
+    const canvas = document.getElementById('student-canvas-5');
+    
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  do {
+  var radius = (prompt("Radius: "))
+  if (radius == null) {
+    break;
+  }
+  if (radius < 32) {
+    alert("Your radius must be at least 32.")
+  }
+  else if (isNaN(radius)) {
+    alert("Your radius is not a number.")
+  }
+  else if (radius > 256) {
+    alert("Your smiley face won't fit on the canvas.")
+  }
+} while (radius > 256 || isNaN(radius) || radius < 32)
+
+var eyesRadius = 0.15 * radius
+var mouthRadius = 0.7 * radius
 
 
-
-    // write your exercise 4 code here
+ctx.beginPath();
+ctx.arc(512, 256, radius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.closePath();
+ctx.beginPath();
+ctx.arc(512, 256, mouthRadius, 0, Math.PI);
+ctx.stroke();
+ctx.closePath();
+ctx.beginPath();
+ctx.arc(512 - 0.4 * radius, 256 - 0.4 * radius, eyesRadius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.closePath();
+ctx.beginPath();
+ctx.arc(512 + 0.4 * radius, 256 - 0.4* radius, eyesRadius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.closePath();
 
 };
 
